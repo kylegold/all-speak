1f30f37c6ae4d0947e5141ba9397ebdb9b366a66
kyle <kyle@kyle.nyc>
2020-12-08 15:25:00 -0500

    changed proxy and path dependency

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const passport = require("../authentication/passport");
 const authRoutes = require("../authentication/authRoutes");
 const apiRoutes = require("./apiRoutes");
 
-Router.use("/auth", authRoutes)
+// Router.use("/auth", authRoutes)
 
 // Router.use("/api", passport.authenticate('jwt', { session: false }), apiRoutes)
 
