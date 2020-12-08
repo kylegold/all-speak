fde2e19b44c7bcb7921f57d4c44e8405490a3120
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-08 18:31:09 -0500

    Added avatar to Sidebar

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
@@ -10,7 +10,7 @@ const Sidebar = () => {
     <div className="sidebar">
       {/* Create a container that will hold all elements of the sidebar header */}
       <div className="sidebar__header">
-
+        <Avatar />
         {/* header right // material-ui needs to be imported to use icons (npm install @material-ui/core) */}
         {/* AFTER material core has been installed, install the icons (npm install @material-ui/icons) */}
         <div className="sidebar__headerRight">
