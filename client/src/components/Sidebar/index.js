43485fca22f2872bae1ee9909e5cbc77678eb437
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-08 18:29:02 -0500

    Added @material-ui icons to Sidebar header

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { IconButton } from '@material-ui/core';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import "./style.css";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>
+      {/* Create a container that will hold all elements of the sidebar header */}
+      <div className="sidebar__header">
+
+        {/* header right // material-ui needs to be imported to use icons (npm install @material-ui/core) */}
+        {/* AFTER material core has been installed, install the icons (npm install @material-ui/icons) */}
+        <div className="sidebar__headerRight">
+          {/* IconButton is a parent component provided by @material-ui that gives the ripple effect when clicked */}
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+
+      </div>
     </div>
   )
 }
