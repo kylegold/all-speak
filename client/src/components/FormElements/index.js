0a4deccc4b383aa06d46ba5c475d7e3bc8d56b6b
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-09 16:16:02 -0500

    Create form elements component file

---
+++ client/src/components/FormElements/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export function BlockBtn(props) {
+    <Button variant="primary" size="lg" block>
+        {props.name}
+    </Button>
+}
+
