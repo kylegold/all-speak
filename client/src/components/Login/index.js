ac400f6589f316723a8a6b744ef474b5bc9394a6
Mike <mike@mikevitelli.com>
2020-12-09 17:32:28 -0500

    changed signin to login

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import Nav from "../Nav/Nav";
+
+const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // get the info from the form
+    console.log(e.target);
+
+    const creds = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    console.log(creds);
+  };
+  return (
+    <>
+      <Nav />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="email" ref={emailRef} />
+        <input type="password" placeholder="password" ref={passwordRef} />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,19 +3,19 @@ import "./style.css";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-	return (
-		<header>
-			<h1>AllSpeak</h1>
-			<ul className="nav">
-				<li>
-					<Link to="/signin">Sign In</Link>
-				</li>
-				<li>
-					<Link to="/signup">Sign Up</Link>
-				</li>
-			</ul>
-		</header>
-	);
+  return (
+    <header>
+      <h1>AllSpeak</h1>
+      <ul className="nav">
+        <li>
+          <Link to="/login">Log In</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+      </ul>
+    </header>
+  );
 };
 
 export default Nav;
