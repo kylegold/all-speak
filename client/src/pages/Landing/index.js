c3f8a70396b92045fb03860c77ea53ce1474e381
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-09 18:52:00 -0500

    Create welcome component and landing page skeleton

---
+++ client/src/pages/Landing/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from '../../components/Welcome';
+
+const Landing = () => {
+    return (
+        <>
+            <Welcome />
+        </>
+    )
+};
+
+export default Landing;
