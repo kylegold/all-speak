63082f24f58f60e2a0d66d9190817f9dd1b182f8
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-08 19:03:19 -0500

    Added icons to Chat/index.js

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import "./style.css";
 
 const Chat = () => {
   return (
     <div className="chat">
-      <h1>I am a chat window</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   )
 }
