c36bec584c3611fad4f882f3f01cb99951508ff0
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-09 15:39:03 -0500

    Removed unnecessary div and cleaned up code

--- client/src/components/SidebarChat/index.js
+++ client/src/components/SidebarChat/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Avatar } from '@material-ui/core';
+import React from "react";
+import { Avatar } from "@material-ui/core";
 import "./style.css";
 
 const SidebarChat = () => {
   return (
-    <div className="app__body">
     <div className="sidebarChat">
       <Avatar />
       <div className="sidebarChat__info">
         <h2>Room name</h2>
         <p>This is the last message</p>
       </div>
-    </div>
     </div>
   )
 }
