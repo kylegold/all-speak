ecc40acf063e4d89576b919398049441511caa0c
rrrossettiii <rrrossettiii@gmail.com>
2020-12-09 15:27:31 -0500

    added crypter

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import ChatApp from './components/ChatApp/index.js';
-import SignIn from './components/SignIn/index.js';
-import SignUp from './components/SignUp/index.js';
+import ChatApp from "./components/ChatApp/index.js";
+import SignIn from "./components/SignIn/index.js";
+import SignUp from "./components/SignUp/index.js";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <Router>
-    <div className="app">
-      {/* <h1>Let's build AllSpeak</h1> */}
-      <Route exact path="/" component={SignIn} />
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-      
-        {/* full chat window with sidebar component */}
-        <Route exact path="/chatroom" component={ChatApp} />
-      
-    </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="app">
+				{/* <h1>Let's build AllSpeak</h1> */}
+				<Route exact path="/" component={SignIn} />
+				<Route exact path="/signin" component={SignIn} />
+				<Route exact path="/signup" component={SignUp} />
+
+				{/* full chat window with sidebar component */}
+				<Route exact path="/chatroom" component={ChatApp} />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,55 +1,50 @@
-import React from 'react';
-import { Avatar, IconButton } from '@material-ui/core';
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
 import { SearchOutlined } from "@material-ui/icons";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import SidebarChat from '../SidebarChat/index.js';
+import SidebarChat from "../SidebarChat/index.js";
 import "./style.css";
-import '../../App.css'
+import "../../App.css";
 
 const Sidebar = () => {
-  return (
-    <div className="app__body">
-    <div className="sidebar">
-      {/* Create a container that will hold all elements of the sidebar header */}
-      <div className="sidebar__header">
-        {/* Add an src with image url to give avatar an actual picture */}
-        <Avatar src="https://i.kym-cdn.com/photos/images/original/001/904/985/621.png"/>
-        {/* header right // material-ui needs to be imported to use icons (npm install @material-ui/core) */}
-        {/* AFTER material core has been installed, install the icons (npm install @material-ui/icons) */}
-        <div className="sidebar__headerRight">
-          {/* IconButton is a parent component provided by @material-ui that gives the ripple effect when clicked */}
-          <IconButton>
-            <DonutLargeIcon />
-          </IconButton>
-          <IconButton>
-            <ChatIcon />
-          </IconButton>
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
-        </div>
-      </div>
+	return (
+		<div className="sidebar">
+			{/* Create a container that will hold all elements of the sidebar header */}
+			<div className="sidebar__header">
+				{/* Add an src with image url to give avatar an actual picture */}
+				<Avatar src="https://i.kym-cdn.com/photos/images/original/001/904/985/621.png" />
+				{/* header right // material-ui needs to be imported to use icons (npm install @material-ui/core) */}
+				{/* AFTER material core has been installed, install the icons (npm install @material-ui/icons) */}
+				<div className="sidebar__headerRight">
+					{/* IconButton is a parent component provided by @material-ui that gives the ripple effect when clicked */}
+					<IconButton>
+						<DonutLargeIcon />
+					</IconButton>
+					<IconButton>
+						<ChatIcon />
+					</IconButton>
+					<IconButton>
+						<MoreVertIcon />
+					</IconButton>
+				</div>
+			</div>
 
-      <div className="sidebar__search">
-        <div className="sidebar__searchContainer">
-          <SearchOutlined />
-          <input 
-            placeholder="Create or search chat"
-            type="text"
-          />
-        </div>
-      </div>
+			<div className="sidebar__search">
+				<div className="sidebar__searchContainer">
+					<SearchOutlined />
+					<input placeholder="Create or search chat" type="text" />
+				</div>
+			</div>
 
-      <div className="sidebar__chats">
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-      </div>
-    </div>
-    </div>
-  )
-}
+			<div className="sidebar__chats">
+				<SidebarChat />
+				<SidebarChat />
+				<SidebarChat />
+			</div>
+		</div>
+	);
+};
 
-export default Sidebar
+export default Sidebar;
