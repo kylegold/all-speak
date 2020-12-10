7f870ae2aadf63ec69b8f2402bc8100215263c38
rrrossettiii <rrrossettiii@gmail.com>
2020-12-10 17:42:25 -0500

    Welcome

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,32 +1,72 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import Welcome from '../../components/Welcome';
-import Logo from '../../assets//logo/png/all_speak_v2_Logo_Black.png';
-import Background from '../../assets/images/adrien-converse-3dUpL1h97CQ-unsplash.jpg'
+import Welcome from "../../components/Welcome";
+import Logo from "../../assets//logo/png/all_speak_v2_Logo_Black.png";
+import Background from "../../assets/images/adrien-converse-3dUpL1h97CQ-unsplash.jpg";
 
 const Landing = () => {
-    return (
-        <div style={{ width: "100vw", height: "100vh", backgroundImage: `url(${Background})`, backgroundSize: "cover" }}>
-            <Container id="container">
-                <Row>
-                    <Col>
-                        <Card className="text-center" id="card">
-                            <Card.Body>
-                            <Link to="/">  <img style={{ width: "50%", minWidth: "200px", display: "block", margin: "0px auto 20px auto" }} src={Logo} alt="allSpeak" /></ Link>
-                                <Welcome />
-                                <div style={{ width: "100%", maxWidth: "570px", marginTop: "10px", marginRight: "auto", marginLeft: "auto" }}>
-                                    <p>Talk to people around the world in a chat room that automatically translates into your spoken language.</p>
-                                </div>
-                               <Link to="/login"> <Button variant="outline-dark" id="button">log in</Button> </Link>{' '}
-                               <Link to="/signup"> <Button variant="dark" id="button">sign up</Button></Link>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: "100vh",
+				backgroundImage: `url(${Background})`,
+				backgroundSize: "cover"
+			}}
+		>
+			<Container id="container">
+				<Row>
+					<Col>
+						<Card className="text-center" id="card">
+							<Card.Body>
+								<Link to="/">
+									{" "}
+									<img
+										style={{
+											width: "50%",
+											minWidth: "200px",
+											display: "block",
+											margin: "0px auto 20px auto"
+										}}
+										src={Logo}
+										alt="allSpeak"
+									/>
+								</Link>
+								<Welcome />
+								<div
+									style={{
+										width: "100%",
+										maxWidth: "570px",
+										marginTop: "10px",
+										marginRight: "auto",
+										marginLeft: "auto"
+									}}
+								>
+									<p>
+										Talk to people around the world in a chat room that
+										automatically translates into your spoken language.
+									</p>
+								</div>
+								<Link to="/login">
+									{" "}
+									<Button variant="outline-dark" id="button">
+										log in
+									</Button>{" "}
+								</Link>{" "}
+								<Link to="/signup">
+									{" "}
+									<Button variant="dark" id="button">
+										sign up
+									</Button>
+								</Link>
+							</Card.Body>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
+		</div>
+	);
 };
 
 export default Landing;
--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Card, Container, Col, Row } from "react-bootstrap";
 import SignUpForm from "../../components/SignUpForm";
+import Welcome from "../../components/Welcome";
 import Background from "../../assets/images/adrien-converse-3dUpL1h97CQ-unsplash.jpg";
 import Logo from "../../assets//logo/png/all_speak_v2_Logo_Black.png";
 // import Nav from "../../components/Nav/Nav";
 
 const SignUp = () => {
-
 	return (
 		<>
 			<div
@@ -23,17 +23,19 @@ const SignUp = () => {
 						<Col>
 							<Card className="text-center" id="card">
 								<Card.Body>
-							 <Link to="/"><img
-										style={{
-											width: "50%",
-											minWidth: "200px",
-											display: "block",
-											margin: "0px auto 20px auto"
-										}}
-										src={Logo}
-										alt="allSpeak"
-									/></Link>									
-                  <Welcome />
+									<Link to="/">
+										<img
+											style={{
+												width: "50%",
+												minWidth: "200px",
+												display: "block",
+												margin: "0px auto 20px auto"
+											}}
+											src={Logo}
+											alt="allSpeak"
+										/>
+									</Link>
+									<Welcome />
 									<SignUpForm />
 								</Card.Body>
 							</Card>
