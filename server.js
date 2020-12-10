55b2271815ac2bdb5b6b26d0b1b954569b20eb8c
kyle <kyle@kyle.nyc>
2020-12-10 15:17:49 -0500

    updated production routing

--- server.js
+++ server.js
@@ -12,10 +12,16 @@ app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get('*', (req,res ) => {
+  (res.sendFile("client/build/index.html"))
+})
 }
 
 app.use("/", routes);
+
+
+  
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/allspeak");
 
