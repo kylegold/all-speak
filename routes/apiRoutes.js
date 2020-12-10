1a7ae125bd76dc3cb76fb0cdd10e6c8e340b09bf
rrrossettiii <rrrossettiii@gmail.com>
2020-12-09 21:51:19 -0500

    push to pull

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,22 @@
 const Router = require("express").Router();
 
 Router.get("/welcome", (req, res) => {
-  res.json({ message: "You have gone full MERN!!!"});
-})
+	res.json({ message: "You have gone full MERN!!!" });
+});
 
-app.post("/user", async (req, res) => {
-  const user = await db.User.create(req.body)
-  res.json(user)
-})
+Router.post("/user", async (req, res) => {
+	const user = await db.User.create(req.body);
+	res.json(user);
+});
 
 Router.get("/users", (req, res) => {
-  db.User.find({}, (err, data) => {
-    if (err) {
-      throw err
-    }
-    else {
-      res.json(data)
-    }
-  })
-})
+	db.User.find({}, (err, data) => {
+		if (err) {
+			throw err;
+		} else {
+			res.json(data);
+		}
+	});
+});
 
 module.exports = Router;
