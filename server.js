3aabc14d87c8d8ca32d971e2196f74476fe4779d
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-11 17:20:47 -0500

    Created routes for sending and syncing messages with chat window

--- server.js
+++ server.js
@@ -31,11 +31,35 @@ app.listen(PORT, () => {
 
 // THIS IS THE DATABASE CONFIGURATION THAT I'M CURRENTLY USING FOR MY MONGODB:
 
-// const connectionUrl = "mongodb+srv://jordanwhunter:P@$$w0rd12345@cluster0.ogmha.mongodb.net/chattingdb?retryWrites=true&w=majority"
+  // const connectionUrl = "mongodb+srv://jordanwhunter:P@$$w0rd12345@cluster0.ogmha.mongodb.net/chattingdb?retryWrites=true&w=majority"
 
-// mongoose.connect(connectionUrl, {
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// })
+  // mongoose.connect(connectionUrl, {
+  //   useCreateIndex: true,
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true
+  // })
+
+// THESE ARE THE API ROUTES I WAS USING TO HIT POSTMAN WITH (note: I imported Messages from my dbMessages.js file which contained a basic messenger schema that I haven't included):
+
+  // app.get("/messages/sync", (req, res) => {
+  //   Messages.find((err, data) => {
+  //     if (err) {
+  //       res.status(500).send(err)
+  //     } else {
+  //       res.status(200).send(data)
+  //     }
+  //   })
+  // });
+
+  // app.get("/messages/new", (req, res) => {
+  //   const dbMessage = req.body
+
+  //   Messages.create(dbMessage, (err, data) => {
+  //     if (err) {
+  //       res.status(500).send(err)
+  //     } else {
+  //       res.status(201).send(`New message created: \n ${data}`)
+  //     }
+  //   })
+  // });
 
