d8b1dad49b22f90d43a00687197fe5e267bbe3dc
Mike <mike@mikevitelli.com>
2020-12-11 13:45:25 -0500

    added preferences page with sample inputs

--- client/src/App.js
+++ client/src/App.js
@@ -23,26 +23,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-	const [state, dispatch] = useGlobalContext();
+  const [state, dispatch] = useGlobalContext();
 
   useEffect(() => {
     checkLogin();
     // loadMessage();
   }, [state.apiToken]);
 
-	const checkLogin = () => {
-		// get the user from local storage
-		const user = JSON.parse(localStorage.getItem("user"));
-		// is there a user?
-		if (user) {
-			// put that user in the state
-			dispatch({
-				type: "LOGIN",
-				email: user.email,
-				apiToken: user.token
-			});
-		}
-	};
+  const checkLogin = () => {
+    // get the user from local storage
+    const user = JSON.parse(localStorage.getItem("user"));
+    // is there a user?
+    if (user) {
+      // put that user in the state
+      dispatch({
+        type: "LOGIN",
+        email: user.email,
+        apiToken: user.token,
+      });
+    }
+  };
 
   // const loadMessage = () => {
   //   axios
@@ -57,34 +57,36 @@ function App() {
   //     });
   // };
 
-	const logout = () => {
-		// remove the user from local storage
-		localStorage.removeItem("user");
-		// remove the user from the state
-		dispatch({ type: "LOGOUT" });
-	};
+  const logout = () => {
+    // remove the user from local storage
+    localStorage.removeItem("user");
+    // remove the user from the state
+    dispatch({ type: "LOGOUT" });
+  };
 
-	return (
-		<div className="App">
-			{/* <h1>Let's build AllSpeak</h1> */}
+  return (
+    <div className="App">
+      {/* <h1>Let's build AllSpeak</h1> */}
 
-			{state.apiToken ? (
-				<Router>
-					<Route exact path="/" component={ChatApp} />
-					<Route exact path="/chatroom" component={ChatApp} />
-					<Route exact path="/chooselanguage" component={ChooseLanguage} />
-					<Route exact path="/preferences" component={Preferences} />
-				</Router>
-			) : (
-				<Router>
-					<Route exact path="/" component={Landing} />
-					<Route path="/landing" component={Landing} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/signup" component={SignUp} />
-				</Router>
-			)}
-		</div>
-	);
+      {state.apiToken ? (
+        <Router>
+          <Route exact path="/" component={ChatApp} />
+          <Route exact path="/chatroom" component={ChatApp} />
+          <Route exact path="/chooselanguage" component={ChooseLanguage} />
+          <Route exact path="/preferences" component={Preferences} />
+        </Router>
+      ) : (
+        <Router>
+          <Route exact path="/" component={Landing} />
+          <Route path="/landing" component={Landing} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/chooselanguage" component={ChooseLanguage} />
+          <Route exact path="/preferences" component={Preferences} />
+        </Router>
+      )}
+    </div>
+  );
 }
 
 export default App;
