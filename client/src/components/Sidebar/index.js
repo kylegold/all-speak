e57f59d37ab053301ca0a87f4f9569c840865d65
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-13 13:06:47 -0500

    Styled sidebar. Added group and private sections for user messages. Added toggle to open and close the sidebar. Incorporated user profile photo to display next to the chat input bar.

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,42 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconButton } from "@material-ui/core";
 import AddIcon from '@material-ui/icons/Add';
 import SidebarChat from "../SidebarChat/index.js";
-import Logo from "../../assets/logo/svg/all_speak_v2_Logo - Black.svg"
 import "./style.css";
+import CloseIcon from '@material-ui/icons/Close';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const Sidebar = () => {
-
+	// Create state to toggle view
+	const [sidebarView, setSidebarView] = useState()
+	// Set the state to opposite of it's current value
+	const renderSidebar = () => setSidebarView(!sidebarView)
 
 	return (
 		<>
-		<div className="sidebar">
-		{/* Create a container that will hold all elements of the sidebar header */}
-		<div className="sidebar__header">
-			{/* Add an src with image url to give avatar an actual picture */}
-			<img src={Logo} style={{width: "150px", marginBottom:"10px"}} alt="allSpeak" />
-			{/* header right // material-ui needs to be imported to use icons (npm install @material-ui/core) */}
-			{/* AFTER material core has been installed, install the icons (npm install @material-ui/icons) */}
-			<div className="sidebar__headerRight">
-				{/* IconButton is a parent component provided by @material-ui that gives the ripple effect when clicked */}
-				<IconButton>
-					<AddIcon />
-				</IconButton>
-			</div>
-		</div>
-
-		<div className="sidebar__search">
-			<div className="sidebar__searchContainer">
-				<input placeholder="Create or search chat" type="text" />
-			</div>
-		</div>
-
-		<div className="sidebar__chats">
-			<SidebarChat />
-			<SidebarChat />
-			<SidebarChat />
-		</div>
-	</div>
+		{/* If sidebar menu is opened, display the sidebar */}
+		{sidebarView ? 
+			<div className="sidebar">
+				{/* Header elements */}
+				<div className="sidebar__header">
+					<div className="sidebar__search">
+						<div className="sidebar__searchContainer">
+							{/* Search bar */}
+							<input style={{width:"90%"}} placeholder="Search" type="text" />
+						</div>
+					</div>
+					<div className="sidebar__headerRight">
+						{/* New chat button */}
+						<IconButton style={{color:"black"}}>
+							<AddIcon />
+						</IconButton>
+						{/* Display either open icon or closed icon depending on the state */}
+						<button style={{color:"black"}} onClick={()=>{renderSidebar()}}>{sidebarView ? <CloseIcon /> : <ArrowBackIosIcon /> }</button>
+					</div>
+				</div>
+				{/* DMs section */}
+				<h3>Private Chats</h3>
+				<div className="sidebar__chats" style={{ marginRight: "15px"}}>
+					<SidebarChat />
+					<SidebarChat />
+					<SidebarChat />
+				</div>
+				{/* Group Chat section */}
+				<h3>Group Chats</h3>
+				<div className="sidebar__chats" style={{ marginRight: "15px"}}>
+					<SidebarChat />
+					<SidebarChat />
+					<SidebarChat />
+				</div>
+			</div> :
+			// If the sidebar is closed
+			<div className="sidebar" style={{justifyContent: "space-between"}}>
+				{/* Show the open button */}
+				<button style={{margin:"30px 0px", padding:"0px"}} onClick={()=>{renderSidebar()}}>{sidebarView ? <CloseIcon /> : <ArrowBackIosIcon />}</button>
+				{/* Display user photo next to the chat input bar */}
+				<div style={{borderRadius: "25px", border: "1px solid black", backgroundColor: "grey", width: "40px", height:"40px"}}>&nbsp;</div>
+			</div>}
 		</>
 	);
 };
