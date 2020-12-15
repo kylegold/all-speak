f34cde132a8d9b07af5875471325ea5e711e0e02
rrrossettiii <rrrossettiii@gmail.com>
2020-12-14 19:23:50 -0500

    Consolidation!

--- client/src/components/PreferencesForm/index.js
+++ client/src/components/PreferencesForm/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import AvatarCropper from "../AvatarCropper/index.jsx";
 import ChangeUsername from "../ChangeUsername";
 import ChangeLanguage from "../ChangeLanguage";
@@ -8,35 +9,37 @@ import "./style.css";
 // import { Avatar } from "@material-ui/core";
 
 const PreferencesForm = () => {
-  return (
-    <>
-      <h1>Preferences</h1>
-      <Row>
-        <Col className="m-auto">
-          {/* avatar cropping module */}
-          <AvatarCropper className="m-a" />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <ChangeLanguage />
-        </Col>
-      </Row>
+	return (
+		<>
+			<h1>Preferences</h1>
+			<Row>
+				<Col className="m-auto">
+					{/* avatar cropping module */}
+					<AvatarCropper className="m-a" />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<ChangeLanguage carousel={false} />
+				</Col>
+			</Row>
 
-      <Row>
-        <Col>
-          <ChangeUsername />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Button variant="dark" type="submit" className="mt-2">
-            Submit
-          </Button>
-        </Col>
-      </Row>
-    </>
-  );
+			<Row>
+				<Col>
+					<ChangeUsername />
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<Link to="/chatroom">
+						<Button variant="dark" type="submit" className="mt-2">
+							Submit
+						</Button>
+					</Link>
+				</Col>
+			</Row>
+		</>
+	);
 };
 
 export default PreferencesForm;
